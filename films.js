6aeedbc281b9a2b063acdd3411e0c0e81deaac5d
roshramos <rosh.ramos30@hotmail.com>
2022-08-26 20:33:02 +1000

    js progress

--- films.js
+++ films.js
@@ -1,22 +1,23 @@
-// 50f9e55e
+// 4c0ff725
 
-//https://api.themoviedb.org/3/movie/550?api_key=10e5b26f65216e0d907d2c1c45c6ea2c
+// http://www.omdbapi.com/?i=tt3896198&apikey=4c0ff725
 
-const movieListEl = document.querySelector("")
-const movie = localStorage.getItem("movie")
+const movieListEl = document.querySelector(".movie__container")
+// const movie = localStorage.getItem("movie")
 
-async function onSearchChange(event) {
-    const movie = event.target.value
-    renderMovies(movie)
-}
+// async function onSearchChange(event) {
+//     const movie = event.target.value
+//     renderMovies(movie)
+// }
 
 async function renderMovies () {
-    const movies = await fetch ('https://api.themoviedb.org/3/movie/550?api_key=10e5b26f65216e0d907d2c1c45c6ea2c')
-    const moviesData = await postMessage.json()
+    const movies = await fetch ('https://www.omdbapi.com/?i=tt3896198&apikey=4c0ff725')
+    const moviesData = await movies.json()
+    movieListEl.innerHTML = 
 }
 
 function movieHTML (movie) {
-    return  <div class="movie--container">
+    return  `<div class="movie--container">
             <div class="movie__contents">
             <div class="movie__poster"></div>
             <div class="movie__info--container">
@@ -24,5 +25,12 @@ function movieHTML (movie) {
                 <div class="movie__year"></div>
             </div>
             </div>
-        </div>
+        </div>`
 }
+
+// const userAction = async () => {
+//     const response = await fetch ('https://www.omdbapi.com/?i=tt3896198&apikey=4c0ff725')
+//     const myJson = await response.json ();
+// }
+
+// console.log (myJson)
