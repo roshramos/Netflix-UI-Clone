b6092af76fafa7aef8337e4b4107a4a3a2b00a36
roshramos <rosh.ramos30@hotmail.com>
2022-08-26 20:08:58 +1000

    js progress

--- films.js
+++ films.js
@@ -1,3 +1,28 @@
 // 50f9e55e
 
 //https://api.themoviedb.org/3/movie/550?api_key=10e5b26f65216e0d907d2c1c45c6ea2c
+
+const movieListEl = document.querySelector("")
+const movie = localStorage.getItem("movie")
+
+async function onSearchChange(event) {
+    const movie = event.target.value
+    renderMovies(movie)
+}
+
+async function renderMovies () {
+    const movies = await fetch ('https://api.themoviedb.org/3/movie/550?api_key=10e5b26f65216e0d907d2c1c45c6ea2c')
+    const moviesData = await postMessage.json()
+}
+
+function movieHTML (movie) {
+    return  <div class="movie--container">
+            <div class="movie__contents">
+            <div class="movie__poster"></div>
+            <div class="movie__info--container">
+                <div class="movie__title"></div>
+                <div class="movie__year"></div>
+            </div>
+            </div>
+        </div>
+}
